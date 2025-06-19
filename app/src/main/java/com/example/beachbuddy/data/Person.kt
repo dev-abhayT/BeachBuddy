ec464aefab2feb2c24453a4d58ec34fffb5d7509
Abhay Tiwari <unknown191617@gmail.com>
2025-06-19 18:48:30 +0530

    Delete master

--- app/src/main/java/com/example/beachbuddy/data/Person.kt
+++
@@ -1,7 +0,0 @@
-package com.example.beachbuddy.data
-
-data class Person(
-    val firstName : String? = "Your Name",
-    val email : String? = "youremail@example.com",
-    val photoUrl : String? = ""
-)
